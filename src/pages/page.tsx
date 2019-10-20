3206064ed10bee1b2ceafd2c76869d7785221fe8
Steff-ee <dsteffee@alumni.stanford.edu>
2019-10-20 13:12:54 -0700

    significantly improving parallax

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,120 +1,123 @@
 import React, { useContext } from 'react'
 // (TODO) fade-in is not hiding the loading bars
-import { Img } from 'react-progressive-loader'
+// import { Img } from 'react-progressive-loader'
 import { NavBar } from '../shared/presentational/components/navBar'
 import { BackgroundsContext } from '../shared/presentational/hooks/useBackgrounds'
 import { ColorsContext } from '../shared/presentational/hooks/useColors'
 
-export interface IHeaderProps {
+export interface IPageProps {
 	titleText: string
-}
-
-export const Header: React.FunctionComponent<IHeaderProps> = (props: IHeaderProps): JSX.Element => {
-	const { titleText } = props
-	const { secondary } = useContext(ColorsContext)
-
-	return (
-		<div
-			style={{
-				backgroundColor: secondary,
-				fontFamily: 'Cabin Sketch',
-				fontSize: '2.5rem',
-				margin: '30px auto',
-				padding: '30px',
-				width: '200px',
-			}}
-		>
-			{titleText}
-		</div>
-	)
-}
-
-export interface IPageProps extends IHeaderProps {
 	Pivots: JSX.Element
 	Content: JSX.Element
 }
 
-const backgroundImgStyle: React.CSSProperties = {
-	display: 'flex',
-	flex: '1 0 auto',
+const parallaxRootStyle: React.CSSProperties = {
+	overflowX: 'hidden',
+	overflowY: 'scroll',
+	perspective: '1px',
+	perspectiveOrigin: '0 0',
+	width: '100vw',
+	height: '100vh',
+	position: 'fixed',
+	top: 0,
+	left: 0,
+	right: 0,
+}
+
+const parallaxGroupStyle: React.CSSProperties = {
+	position: 'relative',
+	transformStyle: 'preserve-3d',
+}
+
+const titleStyle: React.CSSProperties = {
+	fontFamily: 'Cabin Sketch',
+	fontSize: '2.5rem',
+	margin: '30px auto',
+	padding: '30px',
+	width: '200px',
+}
+
+const backgroundStyle: React.CSSProperties = {
 	position: 'absolute',
 	top: 0,
 	left: 0,
 	right: 0,
-	transformOrigin: '0 0',
-	transformStyle: 'preserve-3d',
-	transform: 'translateZ(-1px) scale(2)',
-	zIndex: -1,
+	marginBottom: '120vh',
+}
+
+const pivotsStyle: React.CSSProperties = {
+	margin: '30px auto',
+	width: '500px',
+}
+
+const contentStyle: React.CSSProperties = {
+	margin: '0 auto',
+	maxWidth: '800px',
+	padding: '40px',
 }
 
 export const Page: React.FunctionComponent<IPageProps> = (props: IPageProps): JSX.Element => {
 	const { titleText, Pivots, Content } = props
-	const { primary, accent } = useContext(ColorsContext)
+	const { primary, secondary, accent } = useContext(ColorsContext)
 	const { backgrounds, selectedIndex } = useContext(BackgroundsContext)
 	const background = backgrounds[selectedIndex]
 
 	return (
 		<div
 			style={{
-				width: '100vw',
-				height: '100vh',
-				position: 'fixed',
-				top: 0,
-				left: 0,
-				right: 0,
-				overflowX: 'hidden',
-				overflowY: 'scroll',
-				perspective: '1px',
-				perspectiveOrigin: '0 0',
-				transformStyle: 'preserve-3d',
+				...parallaxRootStyle,
+				fontFamily: 'Lucida Grande, Lucida Sans Unicode, Lucida Sans',
+				textAlign: 'center',
 			}}
 		>
-			<div style={backgroundImgStyle}>
-				<Img src={background.src} style={backgroundImgStyle} />
-			</div>
 			<div
 				style={{
-					display: 'block',
-					position: 'relative',
-					zIndex: 1,
-					fontFamily: 'Lucida Grande, Lucida Sans Unicode, Lucida Sans',
-					marginBottom: '50vh',
-					textAlign: 'center',
+					...parallaxGroupStyle,
+					...backgroundStyle,
+					transformOrigin: '0 0',
+					transform: 'translateZ(-7px) scale(8)',
+					zIndex: -2,
 				}}
 			>
+				<img src={background.src} />
+			</div>
+			<div>
 				<div
 					style={{
 						backgroundColor: accent,
 						position: 'sticky',
 						top: '0px',
 						left: '0px',
+						zIndex: 2,
 					}}
 				>
 					<NavBar rootStyle={{ backgroundColor: accent, position: 'absolute' }} />
 				</div>
-				<div>
-					<Header titleText={titleText} />
-					<div
-						style={{
-							backgroundColor: accent,
-							margin: '30px auto',
-							width: '500px',
-						}}
-					>
-						{Pivots}
-					</div>
-					<div
-						style={{
-							backgroundColor: primary,
-							margin: '0 auto',
-							maxWidth: '800px',
-							padding: '40px',
-						}}
-					>
-						{Content}
-					</div>
+				<div
+					style={{
+						...parallaxGroupStyle,
+						transformOrigin: '0 -25px',
+						transform: 'translateZ(-4px) scale(5)',
+						zIndex: -1,
+					}}
+				>
+					<div style={{ ...titleStyle, backgroundColor: secondary }}>{titleText}</div>
+				</div>
+				<div
+					style={{
+						...parallaxGroupStyle,
+						transformOrigin: '0px -190px',
+						transform: 'translateZ(-1px) scale(2)',
+						zIndex: 1,
+					}}
+				>
+					<div style={{ ...pivotsStyle, backgroundColor: accent }}>{Pivots}</div>
+				</div>
+				<div style={parallaxGroupStyle}>
+					<div style={{ ...contentStyle, backgroundColor: primary }}>{Content}</div>
 				</div>
 			</div>
+			{/* </div> */}
 		</div>
 	)
 }

fed059a12e735e97d93333b4f7f5eea8791859d4
Steff-ee <dsteffee@alumni.stanford.edu>
2019-11-01 22:23:44 -0700

    separating out commands for a future commit

--- src/shared/presentational/components/navBar.tsx
+++ src/shared/presentational/components/navBar.tsx
@@ -2,15 +2,9 @@ import { INavProps } from 'office-ui-fabric-react/lib'
 import React, { useContext } from 'react'
 import { __RouterContext as RouterContext } from 'react-router'
 import { IconLayout, IconNav } from '../../../components/iconNav/iconNav'
-import { gamesTitle } from '../../../pages/games/games'
-import { homeTitle } from '../../../pages/home/home'
-import { mathScienceTitle } from '../../../pages/mathScience/mathScience'
-import { storiesTitle } from '../../../pages/stories/stories'
-import { iconBooksName, iconDieName, iconTeaName, iconTorusKnotName } from '../../helpers/icons'
-import { PageRoutes } from '../../helpers/routes'
-import { Modes, ModesContext } from '../hooks/modeSwitcher'
 import { BackgroundPicker } from './backgroundPicker'
 import { ColorPicker } from './colorPicker'
+import { useChangeModeCommand, useNavigationLinks } from './navBarCommands'
 import { IsNavBarOpenContext } from './navBarHelpers'
 
 export interface INavBarProps {
@@ -20,51 +14,14 @@ export interface INavBarProps {
 
 export const NavBar: React.FunctionComponent<INavBarProps> = (props: INavBarProps): JSX.Element => {
 	const { rootStyle, iconLayout } = props
-	const { history, location } = useContext(RouterContext)
+	const { location } = useContext(RouterContext)
 	const { isNavBarOpen, setIsNavBarOpen } = useContext(IsNavBarOpenContext)
-	const { mode, setMode } = useContext(ModesContext)
-
-	const commonIconStyles = { root: { width: '44px', height: '44px' } }
+	const navigationLinks = useNavigationLinks()
+	const changeModeCommand = useChangeModeCommand()
 
 	const navGroups: INavProps['groups'] = [
 		{
-			links: [
-				{
-					iconProps: { iconName: iconTeaName, styles: commonIconStyles },
-					key: PageRoutes.Home,
-					name: homeTitle,
-					onClick: (): void => history.push({ pathname: PageRoutes.Home }),
-					url: '',
-				},
-				{
-					iconProps: { iconName: iconBooksName, styles: commonIconStyles },
-					key: PageRoutes.Stories,
-					name: storiesTitle,
-					onClick: (): void => history.push({ pathname: PageRoutes.Stories }),
-					url: '',
-				},
-				{
-					iconProps: { iconName: iconDieName, styles: commonIconStyles },
-					key: PageRoutes.Games,
-					name: gamesTitle,
-					onClick: (): void => history.push({ pathname: PageRoutes.Games }),
-					url: '',
-				},
-				{
-					iconProps: { iconName: iconTorusKnotName, styles: commonIconStyles },
-					key: PageRoutes.MathScience,
-					name: mathScienceTitle,
-					onClick: (): void => history.push({ pathname: PageRoutes.MathScience }),
-					url: '',
-				},
-				{
-					iconProps: { iconName: 'Rotate', styles: commonIconStyles },
-					name: 'Mode Toggle',
-					onClick: (): void =>
-						setMode(mode === Modes.Classic ? Modes.Seasons : Modes.Classic),
-					url: '',
-				},
-			],
+			links: [...navigationLinks, changeModeCommand],
 		},
 	]
 
---
+++ src/shared/presentational/components/navBarCommands.tsx
@@ -0,0 +1,58 @@
+import { INavLink } from 'office-ui-fabric-react/lib'
+import { useContext } from 'react'
+import { __RouterContext as RouterContext } from 'react-router'
+import { gamesTitle } from '../../../pages/games/games'
+import { homeTitle } from '../../../pages/home/home'
+import { mathScienceTitle } from '../../../pages/mathScience/mathScience'
+import { storiesTitle } from '../../../pages/stories/stories'
+import { iconBooksName, iconDieName, iconTeaName, iconTorusKnotName } from '../../helpers/icons'
+import { PageRoutes } from '../../helpers/routes'
+import { Modes, ModesContext } from '../hooks/modeSwitcher'
+
+const commonIconStyles = { root: { width: '44px', height: '44px' } }
+
+export const useNavigationLinks = (): INavLink[] => {
+	const { history } = useContext(RouterContext)
+
+	return [
+		{
+			iconProps: { iconName: iconTeaName, styles: commonIconStyles },
+			key: PageRoutes.Home,
+			name: homeTitle,
+			onClick: (): void => history.push({ pathname: PageRoutes.Home }),
+			url: '',
+		},
+		{
+			iconProps: { iconName: iconBooksName, styles: commonIconStyles },
+			key: PageRoutes.Stories,
+			name: storiesTitle,
+			onClick: (): void => history.push({ pathname: PageRoutes.Stories }),
+			url: '',
+		},
+		{
+			iconProps: { iconName: iconDieName, styles: commonIconStyles },
+			key: PageRoutes.Games,
+			name: gamesTitle,
+			onClick: (): void => history.push({ pathname: PageRoutes.Games }),
+			url: '',
+		},
+		{
+			iconProps: { iconName: iconTorusKnotName, styles: commonIconStyles },
+			key: PageRoutes.MathScience,
+			name: mathScienceTitle,
+			onClick: (): void => history.push({ pathname: PageRoutes.MathScience }),
+			url: '',
+		},
+	]
+}
+
+export const useChangeModeCommand = (): INavLink => {
+	const { mode, setMode } = useContext(ModesContext)
+
+	return {
+		iconProps: { iconName: 'Rotate', styles: commonIconStyles },
+		name: 'Mode Toggle',
+		onClick: (): void => setMode(mode === Modes.Classic ? Modes.Seasons : Modes.Classic),
+		url: '',
+	}
+}

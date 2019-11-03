fc0e891e8c410b320d490cb3d7ba41cf6dda5337
Steff-ee <dsteffee@alumni.stanford.edu>
2019-11-03 11:31:59 -0800

    adding small delay to nav hover

--- src/components/iconNav/navItem.tsx
+++ src/components/iconNav/navItem.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { useChangeDelay } from '../../shared/presentational/hooks/useChangeDelay'
 import { ColorsContext } from '../../shared/presentational/hooks/useColors'
 import { INavItem, NavOrientation } from './iconNav.types'
 const ReactHoverObserver = require('react-hover-observer').default
@@ -17,86 +18,108 @@ export type INavItemProps = Omit<INavItem, 'id'> & {
 	orientation: NavOrientation
 }
 
+export type IInnerNavItemProps = INavItemProps & {
+	isHovering: boolean
+}
+
+export const InnerNavItem: React.FunctionComponent<IInnerNavItemProps> = (
+	props: IInnerNavItemProps
+): JSX.Element => {
+	let {
+		labelPosition,
+		icon,
+		onClick,
+		label,
+		width,
+		height,
+		labelWidth,
+		orientation,
+		isHovering,
+	} = props
+	const colors = useContext(ColorsContext)
+
+	const hoverDelay = 150
+	let filter = ''
+	let labelElement: JSX.Element | undefined
+	isHovering = useChangeDelay(isHovering, hoverDelay)
+
+	if (isHovering) {
+		filter = 'invert(0.5)'
+
+		if (labelPosition === NavItemLabelPosition.Hover) {
+			const labelStyle = orientation === NavOrientation.Left ? { right: 0 } : { left: 0 }
+			labelElement = (
+				<div
+					style={{
+						position: 'absolute',
+						display: 'flex',
+						height,
+						marginTop: height,
+						backgroundColor: colors.secondary,
+						color: colors.primary,
+						minWidth: '160px',
+						...labelStyle,
+					}}
+				>
+					<div style={{ margin: 'auto', padding: '0 16px' }}>{label}</div>
+				</div>
+			)
+		}
+	}
+
+	if (labelPosition === NavItemLabelPosition.Right) {
+		labelElement = (
+			<div
+				style={{
+					margin: 'auto 0',
+					textAlign: 'left',
+					paddingLeft: '4%',
+					width: labelWidth,
+					filter,
+				}}
+				onClick={onClick}
+			>
+				{label}
+			</div>
+		)
+	}
+
+	const button = (
+		<div
+			style={{
+				width,
+				height,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				filter,
+			}}
+			onClick={onClick}
+		>
+			{icon}
+		</div>
+	)
+
+	if (labelElement) {
+		return (
+			<div style={{ display: 'flex' }}>
+				{button}
+				{labelElement}
+			</div>
+		)
+	}
+
+	return button
+}
+
 export const NavItem: React.FunctionComponent<INavItemProps> = (
 	props: INavItemProps
 ): JSX.Element => {
-	const { labelPosition, icon, onClick, label, width, height, labelWidth, orientation } = props
-	const colors = useContext(ColorsContext)
-
 	return (
 		<ReactHoverObserver>
-			{({ isHovering }: { isHovering: boolean }): JSX.Element => {
-				let filter = ''
-				let labelElement: JSX.Element | undefined
-				if (isHovering) {
-					filter = 'invert(0.5)'
-
-					if (labelPosition === NavItemLabelPosition.Hover) {
-						const labelStyle =
-							orientation === NavOrientation.Left ? { right: 0 } : { left: 0 }
-						labelElement = (
-							<div
-								style={{
-									position: 'absolute',
-									display: 'flex',
-									height,
-									marginTop: height,
-									backgroundColor: colors.secondary,
-									color: colors.primary,
-									minWidth: '160px',
-									...labelStyle,
-								}}
-							>
-								<div style={{ margin: 'auto', padding: '0 16px' }}>{label}</div>
-							</div>
-						)
-					}
-				}
-
-				if (labelPosition === NavItemLabelPosition.Right) {
-					labelElement = (
-						<div
-							style={{
-								margin: 'auto 0',
-								textAlign: 'left',
-								paddingLeft: '4%',
-								width: labelWidth,
-								filter,
-							}}
-							onClick={onClick}
-						>
-							{label}
-						</div>
-					)
-				}
-
-				const button = (
-					<div
-						style={{
-							width,
-							height,
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							filter,
-						}}
-						onClick={onClick}
-					>
-						{icon}
-					</div>
-				)
-
-				if (labelElement) {
-					return (
-						<div style={{ display: 'flex' }}>
-							{button}
-							{labelElement}
-						</div>
-					)
-				}
-
-				return button
-			}}
+			{({ isHovering }: { isHovering: boolean }): JSX.Element => (
+				<InnerNavItem {...props} isHovering={isHovering} />
+			)}
 		</ReactHoverObserver>
 	)
 }
---
+++ src/shared/presentational/hooks/useChangeDelay.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * Changes from false > true are instant
+ * Changes from true > false are delayed by delayLength milliseconds
+ */
+export const useChangeDelay = (propOn: boolean, delayLength: number): boolean => {
+	const [stateOn, setStateOn] = useState<boolean>(propOn)
+
+	useEffect(() => {
+		if (propOn) {
+			// turning on: instant
+			setStateOn(true)
+		} else {
+			// turning off: delayed
+			setTimeout(() => {
+				setStateOn(false)
+			}, delayLength)
+		}
+	}, [propOn])
+
+	return stateOn || propOn
+}

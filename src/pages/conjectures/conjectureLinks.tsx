39f35602dfdbb8ba9747395274e220c67299e9b6
Steff-ee <dsteffee@alumni.stanford.edu>
2020-01-14 19:06:41 -0800

    Removing accidentally nested links

--- src/pages/conjectures/conjectureLinks.tsx
+++ src/pages/conjectures/conjectureLinks.tsx
@@ -5,33 +5,18 @@ export const ConjectureLinks: React.FunctionComponent = (): JSX.Element => {
 	return (
 		<div>
 			<LinkFrame
-				title={
-					<a href="https://fivethirtyeight.com/" target="_blank">
-						FiveThirtyEight
-					</a>
-				}
+				title={'FiveThirtyEight'}
 				link={'https://fivethirtyeight.com/'}
 				description={`Statistician Nate Silver's news blog`}
 			/>
 			<LinkFrame
-				title={
-					<a href="https://slatestarcodex.com/" target="_blank">
-						Slate Star Codex
-					</a>
-				}
+				title={'Slate Star Codex'}
 				link={'https://slatestarcodex.com/'}
 				description={`Scott Alexander (author of Unsong)'s "rational" blog`}
 				rightShadow={true}
 			/>
 			<LinkFrame
-				title={
-					<a
-						href="https://www.youtube.com/channel/UC0JB7TSe49lg56u6qH8y_MQ/videos"
-						target="_blank"
-					>
-						GDC
-					</a>
-				}
+				title={'GDC'}
 				link={'https://www.youtube.com/channel/UC0JB7TSe49lg56u6qH8y_MQ/videos'}
 				description={'Game Developer Conference presentations'}
 			/>

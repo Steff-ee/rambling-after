350f361710450968f04e16318b130b511f9e449f
Steff-ee <dsteffee@alumni.stanford.edu>
2020-01-21 18:56:41 -0800

    resume continued

--- src/pages/home/resume.tsx
+++ src/pages/home/resume.tsx
@@ -16,31 +16,42 @@ const titleTextStyle: React.CSSProperties = {
 	letterSpacing: '3px',
 }
 
+const subtitleTextStyle: React.CSSProperties = {
+	fontFamily: 'Montserrat',
+	fontSize: '21px',
+	letterSpacing: '1px',
+}
+
+const dividerStar = <div>✧</div>
+
 const ResumeHeader: React.FunctionComponent = (): JSX.Element => {
 	return (
-		<div style={{ textAlign: 'center', marginBottom: '50px' }}>
+		<div style={{ textAlign: 'center', marginBottom: '25px' }}>
 			<div style={titleTextStyle}>DANIEL STEFFEE</div>
-			<div>Software Engineer</div>
+			<div style={subtitleTextStyle}>Software Engineer</div>
 			<div
 				style={{
 					...lightTextStyle,
-					paddingTop: '6px',
+					paddingTop: '12px',
 					display: 'flex',
 					flexDirection: 'row',
 					justifyContent: 'space-between',
 				}}
 			>
 				<div>dsteffee@alumni.stanford.edu</div>
+				{dividerStar}
 				<div>(352) 672-4266</div>
+				{dividerStar}
 				<div>Issaquah, WA</div>
+				{dividerStar}
 				<div>steff-ee.github.io</div>
 			</div>
-			<Divider thickness={2} rootStyle={{ padding: '15px' }} />
+			<Divider thickness={2} rootStyle={{ padding: '12px' }} />
 			<div style={subscriptStyle}>
 				Adaptable Full Stack Web Developer with 5 years’ experience prioritizing the highest
-				impact changes and fixes to customers. Flexibly aided team to ensure at-risk
-				features deliver. Drove design discussions, service reports, and reviewed all code
-				changes to guarantee code quality and product reliability.
+				impact changes for customers. Flexibly aided team to ensure at-risk features
+				deliver. Drove design discussions, service reports, and reviewed all code changes to
+				guarantee code quality and product reliability.
 			</div>
 		</div>
 	)
@@ -59,7 +70,7 @@ const ResumeSection: React.FunctionComponent<IResumeSectionProps> = (
 	return (
 		<div
 			style={{
-				marginBottom: '30px',
+				marginBottom: '25px',
 			}}
 		>
 			<div
@@ -67,11 +78,46 @@ const ResumeSection: React.FunctionComponent<IResumeSectionProps> = (
 					fontFamily: 'Montserrat',
 					fontSize: '23px',
 					textAlign: 'center',
+					marginBottom: '15px',
 				}}
 			>
 				{title}
 			</div>
 			<div style={lightTextStyle}>{body}</div>
+		</div>
+	)
+}
+
+interface IExperienceSubSectionProps {
+	title: string
+	timeRange: string
+	body: JSX.Element
+}
+
+const ExperienceSubSection: React.FunctionComponent<IExperienceSubSectionProps> = (
+	props: IExperienceSubSectionProps
+): JSX.Element => {
+	const { title, timeRange, body } = props
+
+	return (
+		<div style={{ marginBottom: '25px' }}>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-between',
+				}}
+			>
+				<div
+					style={{
+						fontWeight: 600,
+						fontSize: '21px',
+					}}
+				>
+					{title}
+				</div>
+				<div>{timeRange}</div>
+			</div>
+			<div style={{ marginTop: '20px' }}>{body}</div>
 		</div>
 	)
 }
@@ -95,9 +141,45 @@ export const Resume: React.FunctionComponent = (): JSX.Element => {
 			}}
 		>
 			<ResumeHeader />
-			<ResumeSection title={'WORK EXPERIENCE'} body={<>Blah blah blah</>} />
-			<ResumeSection title={'EDUCATION'} body={<>Blah blah blah</>} />
-			<ResumeSection title={'SKILLS'} body={<>Blah blah blah</>} />
+			<ResumeSection
+				title={'WORK EXPERIENCE'}
+				body={
+					<>
+						<ExperienceSubSection
+							title={'Microsoft'}
+							timeRange={'2018 - present'}
+							body={<>Blah blah blah</>}
+						/>
+						<ExperienceSubSection
+							title={'TreeRing'}
+							timeRange={'2015 - 2018'}
+							body={<>Blah blah blah</>}
+						/>
+					</>
+				}
+			/>
+			<ResumeSection
+				title={'EDUCATION'}
+				body={
+					<ExperienceSubSection
+						title={'Stanford'}
+						timeRange={'2010 - 2015'}
+						body={<>Blah blah blah</>}
+					/>
+				}
+			/>
+			<ResumeSection
+				title={'SKILLS'}
+				body={
+					<>
+						<li>
+							Currently Proficient: React, Redux, GraphQL, TypeScript, JavaScript,
+							MySQL
+						</li>
+						<li>Prior Proficiencies: Angular 4, PHP, Java, C, C++, Ruby</li>
+					</>
+				}
+			/>
 		</div>
 	)
 }

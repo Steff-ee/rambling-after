097c5216e38caf716a8d4d6c7a371fd3fcb35b2e
Steff-ee <dsteffee@alumni.stanford.edu>
2020-03-28 20:22:30 -0700

    Updating photo choices for summer spring and autumn

--- src/shared/helpers/artists.ts
+++ src/shared/helpers/artists.ts
@@ -1,4 +1,3 @@
-import sarrailhAutumnImg0 from 'Assets/images/sarrailh_autumn_0.png'
 import sarrailhAutumnImg1 from 'Assets/images/sarrailh_autumn_1.png'
 
 export interface IPicture {
@@ -34,6 +33,10 @@ export const Mertun = {
 export const Rapt = { artistName: 'Rapt', artistLink: 'https://raptjp.tumblr.com/' }
 export const Liu = { artistName: 'Chao Liu', artistLink: 'https://www.artstation.com/chaoliu' }
 export const Fadeev = { artistName: 'Anton Fadeev', artistLink: 'https://www.artstation.com/shant' }
+export const Schilde = {
+	artistName: 'Maxime Schilde',
+	artistLink: 'https://www.artstation.com/maximeschilde',
+}
 
 // "Royalty Free"
 export const wallpaperFlare = {
@@ -43,21 +46,28 @@ export const wallpaperFlare = {
 
 // to find the original link, grab XXXX from "wallhaven-XXXX.jpg" and insert into https://wallhaven.cc/w/XXXX
 export const springBackgrounds: IPicture[] = [
-	{ src: 'https://w.wallhaven.cc/full/p8/wallhaven-p8yr1j.jpg', ...Liu },
 	{ src: 'https://w.wallhaven.cc/full/qd/wallhaven-qdpx2r.jpg', ...Rapt },
-	{ src: 'https://w.wallhaven.cc/full/md/wallhaven-mdo9o8.png', ...Sarrailh },
+	{ src: 'https://w.wallhaven.cc/full/p8/wallhaven-p8yr1j.jpg', ...Liu },
 	{ src: 'https://w.wallhaven.cc/full/p8/wallhaven-p8j3g3.jpg', ...JoeyJazz },
 	{ src: 'https://w.wallhaven.cc/full/4o/wallhaven-4opl79.jpg', ...Gercken },
 ]
 export const summerBackgrounds: IPicture[] = [
-	{ src: 'https://w.wallhaven.cc/full/96/wallhaven-96km7k.png', ...Sarrailh },
 	{ src: 'https://w.wallhaven.cc/full/g8/wallhaven-g817ve.png', ...Sarrailh },
 	{ src: 'https://w.wallhaven.cc/full/r2/wallhaven-r2ed5w.png', ...Sarrailh },
-	{ src: 'https://w.wallhaven.cc/full/r2/wallhaven-r2ed5w.png', ...Sarrailh }, // (TODO) find another
+	{ src: 'https://w.wallhaven.cc/full/96/wallhaven-96km7k.png', ...Sarrailh },
+	{
+		src:
+			'https://cdna.artstation.com/p/assets/images/images/018/561/748/large/sylvain-sarrailh-melodiousbreeze02.jpg',
+		...Sarrailh,
+	},
 ]
 export const autumnBackgrounds: IPicture[] = [
-	{ src: sarrailhAutumnImg0, ...Sarrailh },
 	{ src: sarrailhAutumnImg1, ...Sarrailh },
+	{
+		src:
+			'https://cdnb.artstation.com/p/assets/images/images/013/185/961/large/maxime-schilde-autumn-winds-04.jpg',
+		...Schilde,
+	},
 	{ src: 'https://w.wallhaven.cc/full/76/wallhaven-76pv3v.jpg', ...Grivet },
 	{ src: 'https://w.wallhaven.cc/full/4y/wallhaven-4y8p9l.jpg', ...Gercken },
 ]

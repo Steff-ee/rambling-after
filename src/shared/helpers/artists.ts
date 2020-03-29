22440444320e6fbbecfaa6c5bc4be10ad54a26bd
Steff-ee <dsteffee@alumni.stanford.edu>
2020-03-28 20:01:59 -0700

    Swapping winter photos

--- src/shared/helpers/artists.ts
+++ src/shared/helpers/artists.ts
@@ -27,12 +27,18 @@ export const JoeyJazz = {
 	artistName: 'JoeyJazz',
 	artistLink: 'https://www.deviantart.com/joeyjazz',
 }
+export const Mertun = {
+	artistName: 'ömer tunç',
+	artistLink: 'https://www.artstation.com/mertun',
+}
 export const Rapt = { artistName: 'Rapt', artistLink: 'https://raptjp.tumblr.com/' }
 export const Liu = { artistName: 'Chao Liu', artistLink: 'https://www.artstation.com/chaoliu' }
 export const Fadeev = { artistName: 'Anton Fadeev', artistLink: 'https://www.artstation.com/shant' }
-export const alartstudio = {
-	artistName: 'alartstudio',
-	artistLink: 'https://www.deviantart.com/alartstudio',
+
+// "Royalty Free"
+export const wallpaperFlare = {
+	artistName: 'Wallpaper Flare',
+	artistLink: 'https://www.wallpaperflare.com/',
 }
 
 // to find the original link, grab XXXX from "wallhaven-XXXX.jpg" and insert into https://wallhaven.cc/w/XXXX
@@ -58,6 +64,6 @@ export const autumnBackgrounds: IPicture[] = [
 export const winterBackgrounds: IPicture[] = [
 	{ src: 'https://w.wallhaven.cc/full/g8/wallhaven-g8ml1q.jpg', ...Riebe },
 	{ src: 'https://i.imgur.com/41J50g7.jpg', ...Fadeev },
-	{ src: 'https://w.wallhaven.cc/full/lq/wallhaven-lqdrdy.jpg', ...alartstudio },
-	{ src: 'https://w.wallhaven.cc/full/lq/wallhaven-lqdrdy.jpg', ...alartstudio }, // (TODO) find another
+	{ src: 'https://w.wallhaven.cc/full/96/wallhaven-96zxjw.jpg', ...Mertun },
+	{ src: 'https://w.wallhaven.cc/full/76/wallhaven-7671ey.png', ...wallpaperFlare },
 ]
